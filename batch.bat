5bbbfd9be3a27c01173f0d7e04a444f4434ff990
leeyh.lee <leeyh.lee@zkteco.com>
2017-12-14 10:44:12 +0800

    updat build.gradle

--- batch.bat
+++ batch.bat
@@ -1,6 +1,6 @@
 @ echo off
 @ echo "pack project: app"
-call gradlew.bat clean assembleRelease -p app
+call gradlew.bat clean appRelease -p app
 @ echo "pack project: plugin-one"
 call gradlew.bat clean assemblePlugin -p plugin-one
 @ echo.
