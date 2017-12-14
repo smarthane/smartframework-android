0af62c61eac826e2fc6c2aae933ad3919643141a
leeyh.lee <leeyh.lee@zkteco.com>
2017-12-14 11:37:36 +0800

    updat batch.bat

--- batch.bat
+++ batch.bat
@@ -1,8 +1,8 @@
 @ echo off
-@ echo "pack project: app"
+@ echo "step one.pack project: app"
 call gradlew.bat clean appRelease -p app
-@ echo "pack project: plugin-one"
+@ echo "step two.pack project: plugin-one"
 call gradlew.bat clean assemblePlugin -p plugin-one
-@ echo "pack project: zipRelease"
+@ echo "step three.pack release: zipRelease"
 call gradlew.bat zipRelease
 @ echo.
